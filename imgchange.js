1fe79808bdcb8ad3dabc978fbd40681c8693bb07
Amitay Rodrigues <bamitay092@gmail.com>
2024-11-24 16:26:37 +0000

    2.0

--- imgchange.js
+++ imgchange.js
@@ -1,5 +1,5 @@
 const imgArray = ['./images/sheeps.webp', './images/Costa_Oeste.webp', './images/picton_castle_interior.webp', './images/praia_gales.webp', './images/bath_bridge_falls.webp'];
-const dymImg = document.getElementById('dym_img')
+const  dymImg= document.getElementById('dym_img')
 
 let index = 0;
 
--- script.js
+++ script.js
@@ -15,3 +15,19 @@ const nav = document.querySelector(".nav");
 hamburguer.addEventListener("click", () => {
     nav.classList.toggle("active")
 })
+
+const txtBtn = document.getElementById('button_change_text');
+const textBox = document.getElementById('text_changed');
+const textArray = [
+    "Welsh Cake(bolo galês), meio entre um bolo e um biscoito. Ele é feito com ingredientes simples como farinha, açúcar, manteiga, ovos e passas, mas o segredo está na forma decozimento em uma frigideira especial com ranhuras.",
+    "Welsh Lamb (Cordeiro Galês)O País de Gales é famoso por seus pastos verdes e por produzir um dos melhores cordeiros do mundo. O Welsh Lamb é conhecido por sua carne macia, saborosa e com pouca gordura.",
+    "Cawl(sopa ou caldo), O Cawl é considerado o prato nacional do País de Gales. É um ensopado reconfortante, feito com carne de cordeiro, alho-poró e legumes da estação. O Cawl é um prato simples, mas cheio de sabor, perfeito para aquecer nos dias frios.",
+    "Nessa parte que pode assustar ou animar dependendo da pessoa, no meu caso foi bem animador. Confesso que fiquei com receio de comer o famoso feijão doce daqui, mas acabou que eu gostei. Com as comidas típicas de Gales, a conversa foi outra, clique nas setas para ver mais sobre."
+]
+
+let textIndex = 0;
+
+txtBtn.addEventListener("click", () =>{
+    textIndex = (textIndex+1) % textArray.length
+    textBox.textContent = textArray[textIndex]
+})
